ba672ec82e5899aac36639c235b8695e9b677bd4
Paweł Tatarynowicz <pawel.tatarynowicz@monterail.com>
2021-08-24 11:05:18 +0200

    Add postcode to vendor

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_10_140732) do
+ActiveRecord::Schema.define(version: 2021_08_24_072405) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -1267,6 +1267,7 @@ ActiveRecord::Schema.define(version: 2021_08_10_140732) do
     t.float "commission_rate", default: 5.0
     t.integer "priority"
     t.string "notification_email"
+    t.string "postcode"
     t.index ["deleted_at"], name: "index_spree_vendors_on_deleted_at"
     t.index ["name"], name: "index_spree_vendors_on_name", unique: true
     t.index ["slug"], name: "index_spree_vendors_on_slug", unique: true
