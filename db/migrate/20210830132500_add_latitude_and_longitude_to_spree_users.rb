ce1b0c731e9bcb4405a16e256c374b4b394fd664
Paweł Tatarynowicz <pawel.tatarynowicz@monterail.com>
2021-09-01 22:31:15 +0200

    add search_scope

--- db/migrate/20210830132500_add_latitude_and_longitude_to_spree_users.rb
+++
@@ -1,6 +0,0 @@
-class AddLatitudeAndLongitudeToSpreeUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :spree_users, :latitude, :float
-    add_column :spree_users, :longitude, :float
-  end
-end
