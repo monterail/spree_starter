ad05f84121cbf950ddd26586d55cdd56f2774b96
Paweł Tatarynowicz <pawel.tatarynowicz@monterail.com>
2021-10-01 09:02:45 +0200

    Debbuging calculator

--- app/models/spree/calculator/shipping/store_distance.rb
+++ app/models/spree/calculator/shipping/store_distance.rb
@@ -4,10 +4,10 @@ require_dependency 'spree/shipping_calculator'
 module Spree
   module Calculator::Shipping
     class StoreDistance < Spree::ShippingCalculator
-      preference :base_amount_up_to_2_5_mi, :decimal, default: 0
-      preference :base_amount_up_to_3_5_mi, :decimal, default: 0
-      preference :base_amount_up_to_6_mi, :decimal, default: 0
-      preference :base_amount_up_to_9_mi, :decimal, default: 0
+      preference :base_amount_up_to_2_5_mi, :decimal, default: 2.5
+      preference :base_amount_up_to_3_5_mi, :decimal, default: 3.5
+      preference :base_amount_up_to_6_mi, :decimal, default: 4.5
+      preference :base_amount_up_to_9_mi, :decimal, default: 5.5
 
       def self.description
         Spree.t(:shipping_store_distance)
@@ -17,17 +17,72 @@ module Spree
         compute_store_distance(package)
       end
 
-      delegate :compute_store_distance, to: :distance_calculator
+      def vendors_coordinates(package)
+        package.order.variants.map do |variant|
+          variant.vendor.to_coordinates
+        end
+      end
 
-      private
+      def total_product_weight(package)
+        total_weight = 0
 
-      def distance_calculator
-        ::Spree::Calculator::StoreDistance.new(
-          preferred_base_amount_up_to_2_5_mi: preferred_base_amount_up_to_2_5_mi,
-          preferred_base_amount_up_to_3_5_mi: preferred_base_amount_up_to_3_5_mi,
-          preferred_base_amount_up_to_6_mi: preferred_base_amount_up_to_6_mi,
-          preferred_base_amount_up_to_9_mi: preferred_base_amount_up_to_9_mi
-        )
+        total_weight = package.order.variants.map do |variant|
+          variant.weight
+        end
+        total_weight.sum
+      end
+
+      def total_distance(package)
+        vendor_co = vendors_coordinates(package)
+
+        total_distance = 0
+
+        total_distance += Geocoder::Calculations.distance_between(package.stock_location.to_coordinates, vendor_co[0])
+
+        vendor_co[0...-1].each_with_index do |val, index|
+          total_distance += Geocoder::Calculations.distance_between(vendor_co[index], vendor_co[index + 1])
+        end
+
+        total_distance += Geocoder::Calculations.distance_between(vendor_co.last, package.order.ship_address.to_coordinates)
+
+        total_distance += Geocoder::Calculations.distance_between(package.order.ship_address.to_coordinates, package.stock_location.to_coordinates)
+      end
+
+      def store_collection_charge(package)
+        vendor_co = vendors_coordinates(package)
+        vendor_co.inject(-0.5) { |sum, number| sum + 0.5}
+      end
+
+      def compute_store_distance(package)
+        binding.pry
+
+        milage_price_band = total_distance(package)
+
+        case milage_price_band
+        when 0..2.5
+          milage_price_band = base_amount_up_to_2_5_mi
+        when 2.6..3.5
+          milage_price_band = base_amount_up_to_3_5_mi
+        when 3.6..5.9
+          milage_price_band = preferred_base_amount_up_to_6_mi
+        when 6.0..9
+          milage_price_band = preferred_base_amount_up_to_9_mi
+        end
+
+        scc = store_collection_charge(package)
+
+        total_weight = total_product_weight(package)
+
+        case total_weight
+        when 0..25
+          total_weight = 1
+        when 26..36
+          total_weight = 1.2
+        when 36..50
+          total_weight = 1.4
+        end
+
+        total_price = (milage_price_band * total_weight) + scc
       end
     end
   end
--- spec/models/spec/calculator/shipping/store_distance_spec.rb
+++ spec/models/spec/calculator/shipping/store_distance_spec.rb
@@ -9,10 +9,11 @@ module Spree
       let(:variant2) { build(:variant, price: 25) }
 
       let(:package) do
-        build(:stock_package, variants_contents: {variant1 => 5})
+        build(:stock_package, variants_contents: { variant1 => 5 })
       end
-      let!(:vendor) { Spree::Vendor.create!(name: "vendor1", about_us: "50 Newton St, Birmingham B4 6NE") }
-      let!(:vendor2) { Spree::Vendor.create!(name: "vendor2", about_us: "Ryder St, Birmingham B4 7NE") }
+
+      # let!(:vendor) { Spree::Vendor.create!(name: "vendor1", about_us: "50 Newton St, Birmingham B4 6NE") }
+      # let!(:vendor2) { Spree::Vendor.create!(name: "vendor2", about_us: "Ryder St, Birmingham B4 7NE") }
 
       let(:stock_location1) { build(:stock_location) }
 
@@ -23,9 +24,10 @@ module Spree
         expect(subject.compute(package)).to eq(0)
       end
 
-      it 'return vendor' do
-        expect(vendor.to_coordinates).to eq(Spree::Vendor.first.to_coordinates)
-      end
+      # Bez sensu
+      #it 'return vendor' do
+      # expect(vendor.to_coordinates).to eq(Spree::Vendor.first.to_coordinates)
+      #end
 
       it 'allows creation of new object with all the attributes' do
         StoreDistance.new(
