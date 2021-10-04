98eb96ce7b03921ff744df34ff564b5a6e768af2
Paweł Tatarynowicz <pawel.tatarynowicz@monterail.com>
2021-10-04 11:07:01 +0200

    calculator correction

--- app/models/spree/calculator/shipping/store_distance.rb
+++ app/models/spree/calculator/shipping/store_distance.rb
@@ -24,65 +24,53 @@ module Spree
       end
 
       def total_product_weight(package)
-        total_weight = 0
-
         total_weight = package.order.variants.map do |variant|
           variant.weight
         end
-        total_weight.sum
+        total_load = total_weight.inject(0) { |sum, number| sum + number }
+
+        case total_load
+        when 0..25 then total_load = 1
+        when 26..36 then total_load = 1.2
+        when 36..50 then total_load = 1.4
+        end
       end
 
       def total_distance(package)
         vendor_co = vendors_coordinates(package)
 
-        total_distance = 0
+        milage_price_band = 0
 
-        total_distance += Geocoder::Calculations.distance_between(package.stock_location.to_coordinates, vendor_co[0])
+        vendor_co.unshift(package.stock_location.to_coordinates)
+        vendor_co << package.order.ship_address.to_coordinates
+        vendor_co << package.stock_location.to_coordinates
 
         vendor_co[0...-1].each_with_index do |val, index|
-          total_distance += Geocoder::Calculations.distance_between(vendor_co[index], vendor_co[index + 1])
+          milage_price_band += Geocoder::Calculations.distance_between(vendor_co[index], vendor_co[index + 1])
         end
 
-        total_distance += Geocoder::Calculations.distance_between(vendor_co.last, package.order.ship_address.to_coordinates)
-
-        total_distance += Geocoder::Calculations.distance_between(package.order.ship_address.to_coordinates, package.stock_location.to_coordinates)
+        case milage_price_band
+        when 0..2.5 then milage_price_band = 2.5
+        when 2.6..3.5 then milage_price_band = 3.5
+        when 3.6..5.9 then milage_price_band = 4.5
+        when 6.0..9 then milage_price_band = 5.5
+        else raise "We do not deliver further than 9 miles"
+        end
       end
 
       def store_collection_charge(package)
-        vendor_co = vendors_coordinates(package)
-        vendor_co.inject(-0.5) { |sum, number| sum + 0.5}
+        vendors_coordinates(package).inject(-0.5) { |sum, number| sum + 0.5 }
       end
 
       def compute_store_distance(package)
         binding.pry
-
         milage_price_band = total_distance(package)
 
-        case milage_price_band
-        when 0..2.5
-          milage_price_band = base_amount_up_to_2_5_mi
-        when 2.6..3.5
-          milage_price_band = base_amount_up_to_3_5_mi
-        when 3.6..5.9
-          milage_price_band = preferred_base_amount_up_to_6_mi
-        when 6.0..9
-          milage_price_band = preferred_base_amount_up_to_9_mi
-        end
-
-        scc = store_collection_charge(package)
+        store_collection_charge(package)
 
         total_weight = total_product_weight(package)
 
-        case total_weight
-        when 0..25
-          total_weight = 1
-        when 26..36
-          total_weight = 1.2
-        when 36..50
-          total_weight = 1.4
-        end
-
-        total_price = (milage_price_band * total_weight) + scc
+        (milage_price_band * total_weight) + store_collection_charge(package)
       end
     end
   end
